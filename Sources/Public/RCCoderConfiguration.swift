f7e519f36b9e6cdc0cdf7b33ca7b84a764d9eeb6
Marius Ilie <mariusjcb@users.noreply.github.com>
2020-09-11 18:32:59 +0300

    Update RCCoderConfiguration.swift

--- Sources/Public/RCCoderConfiguration.swift
+++ Sources/Public/RCCoderConfiguration.swift
@@ -64,19 +64,19 @@ public extension RCCoderConfiguration {
     
     var topLevelBitesCount: Int {
       switch self {
-        case .v1: return 8
+        case .v1: return 24
       }
     }
     
     var middleLevelBitesCount: Int {
       switch self {
-        case .v1: return 8
+        case .v1: return 24
       }
     }
     
     var bottomLevelBitesCount: Int {
       switch self {
-        case .v1: return 8
+        case .v1: return 24
       }
     }
     
